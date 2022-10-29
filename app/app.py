09b136b129a8f0772a4efdd9b780bc0c5a20d6f7
Moukatech <lewismocha@gmail.com>
2022-10-29 17:42:48 +0300

    updated corsmiddleware

--- app/app.py
+++ app/app.py
@@ -16,12 +16,13 @@ app.include_router(donate.router)
 
 origins = [
     "http://127.0.0.1:5500",
+    "kitfest.co.ke"
 ]
 
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
